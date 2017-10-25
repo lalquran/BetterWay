869ff2767014966c9d2b2c4924b961cc6487d40c
Gideon O. Ubaldo <gideon.ubaldo@sjsu.edu>
2017-10-25 12:35:31 -0700

    deleted ruby

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#index'
-
-  get 'pages/account'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
